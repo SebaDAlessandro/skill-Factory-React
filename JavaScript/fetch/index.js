40cbc85bd4ee6dbd39d8e49ccf2a186b4dda3f22
Seba <sebadalessandro@hotmail.com>
2022-08-03 10:30:31 -0300

    :bulb:JS functions are removed and a ternary is added to the main class of the card

--- JavaScript/fetch/index.js
+++ JavaScript/fetch/index.js
@@ -5,52 +5,18 @@ fetch(url)
 
 const mostrarData = (data) => {
 
-    const divTitle = document.getElementById('title')
-    const dashboard = document.createElement('div')
-    dashboard.classList.add('dashboard')
-
-    for (let i = 0; i < data.length; i++) {
-
-        if(data[i].completed){
-            dashboard.innerHTML += cardTrue(data[i])
-        }else{
-            dashboard.innerHTML += cardFalse(data[i])
-        }
-
-    }
-
-    divTitle.appendChild(dashboard)
-
-}
-
-function cardTrue(element){
-
-    const card = 
-        `<div class="card">
-            <h3 class="id">Id: ${element.id}</h3>
-                <div class="detail">
-                <p class="userId">User-Id: ${element.userId}</p>
-                <p class="title">Title: ${element.title}</p>
-                <p class="state">Estate: ${element.completed}</p>
-            </div>
+    let card = ''
+    data.map((element) => {
+        card += `<div class=${element.completed ? 'cardTrue' : 'cardFalse'} id='tarjetaId'>
+        <h3 class="id">${element.id}</h3>
+        <div class="detail">
+        <p class="userId">User-Id: ${element.userId}</p>
+        <p class="title">Title: ${element.title}</p>
+        <p class=${element.completed ? 'state' : 'stateF'}>Estate: ${element.completed}</p>
+        </div>
         </div>`
 
-    return card
-
+        document.getElementById("containerCards").innerHTML = card
+    })
 }
 
-function cardFalse(element){
-
-    const card = 
-    `<div class="card2">
-        <h3 class="id">Id: ${element.id}</h3>
-            <div class="detail">
-            <p class="userId">User-Id: ${element.userId}</p>
-            <p class="title">Title: ${element.title}</p>
-            <p class="state">Estate: ${element.completed}</p>
-        </div>
-    </div>`
-
-return card
-
-}
