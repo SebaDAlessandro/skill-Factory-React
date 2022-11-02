054c3bb4e73bddeedab944a99e5e9427681c356f
Seba <sebadalessandro@hotmail.com>
2022-11-02 13:27:56 -0300

    sf test

---
+++ test-app/src/components/MainComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainComponent = () => {
+  return (
+    <div>
+        <input
+            type='text'
+            placeholder='input area'
+            title='skill input'
+            id='input'
+        />
+    </div>
+  )
+}
+
+export default MainComponent
